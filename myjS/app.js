ed5fdc4dfdd991f32187023a00bf4a7c25b3e56a
sofai4 <lagosmorgan0@gmail.com>
2023-10-02 14:20:51 +0100

    go

--- myjS/app.js
+++ myjS/app.js
@@ -8,27 +8,27 @@ function red() {
     if(opt.value == 'cours') {
 
  
-        window.location.href = "download/cours.html";
+        window.location.href = "https://sofai4.github.io/home/download/cours.html";
         
         }
         
         if(opt.value == 'games') {
         
-            window.location.href = "/download/games.html";
+            window.location.href = "https://sofai4.github.io/home/download/games.html";
         
         }
         
         
         if(opt.value == 'doc') {
         
-            window.location.href = "/download/doc.html";
+            window.location.href = "https://sofai4.github.io/home/download/doc.html";
         
         }
         
         
         if(opt.value == 'res') {
         
-            window.location.href = "/download/res.html";
+            window.location.href = "https://sofai4.github.io/home/download/res.html";
         
         }
 
