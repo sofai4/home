8aaf79c52917859cd44617561817f29db9086f02
sofai4 <lagosmorgan0@gmail.com>
2024-04-30 05:09:11 -0700

    this just for format the code

--- morgan4h/calc/app.js
+++ morgan4h/calc/app.js
@@ -1,29 +1,28 @@
-let num1 = document.querySelectorAll('input'),
-    num2 = document.querySelectorAll('input'),
-    p    = document.querySelector('.p'),
-    m    = document.querySelector('.m'),
-    mul  = document.querySelector('.mul'),
-    d    = document.querySelector('.d'),
-    res  = document.querySelector('.res');
+let num1 = document.querySelectorAll("input"),
+  num2 = document.querySelectorAll("input"),
+  p = document.querySelector(".p"),
+  m = document.querySelector(".m"),
+  mul = document.querySelector(".mul"),
+  d = document.querySelector(".d"),
+  res = document.querySelector(".res");
 
+p.onclick = function () {
+  // console.log(parseInt(num1[0].value) + parseInt(num2[1].value))
+  res.textContent = parseInt(num1[0].value) + parseInt(num2[1].value);
+  location.href = "#r";
+};
 
-p.onclick = function() {
-    // console.log(parseInt(num1[0].value) + parseInt(num2[1].value))
-    res.textContent = parseInt(num1[0].value) + parseInt(num2[1].value)
-    location.href = "#r";
-}
+m.onclick = function () {
+  res.textContent = parseInt(num1[0].value) - parseInt(num2[1].value);
+  location.href = "#r";
+};
 
-m.onclick = function() {
-    res.textContent = parseInt(num1[0].value) - parseInt(num2[1].value)
-    location.href = "#r";
-}
+mul.onclick = function () {
+  res.textContent = parseInt(num1[0].value) * parseInt(num2[1].value);
+  location.href = "#r";
+};
 
-mul.onclick = function() {
-    res.textContent = parseInt(num1[0].value) * parseInt(num2[1].value)
-    location.href = "#r";
-}
-
-d.onclick = function() {
-    res.textContent = parseInt(num1[0].value) / parseInt(num2[1].value)
-    location.href = "#r";
-}
+d.onclick = function () {
+  res.textContent = parseInt(num1[0].value) / parseInt(num2[1].value);
+  location.href = "#r";
+};
