ae62c9ac3733c9517bb19eeb55169c2a6def4647
sofai4 <lagosmorgan0@gmail.com>
2024-05-06 00:41:27 +0100

    add time for function of anim by the way the anim with jequery

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log("file Js Working...")
 
 $().ready(function() {
-    $('h1').hide(100,function() {
-        $('h1').show(200) 
+    $('h1').hide(1000,function() {
+        $('h1').show(2000) 
     })
 })
