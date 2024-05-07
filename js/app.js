fe5f4ee619f3fb549ece64420f9bc4311b3f037f
sofai4 <lagosmorgan0@gmail.com>
2024-05-07 22:05:02 +0100

    add html code for home and some coding for nav with css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-console.log("file Js Working...")
-
 $().ready(function() {
-    $('h1').hide(1000,function() {
-        $('h1').show(2000) 
-    })
+    // console.log("hello world")
+    $('img').show(800)
 })
