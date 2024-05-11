50b403c2ba537428d746990602f8a2fa18ea36d7
sofai4 <lagosmorgan0@gmail.com>
2024-05-11 16:13:55 +0100

    complete all the basic of html and css code in index.html and maybe we gona change some color layot in the futur

--- js/app.js
+++ js/app.js
@@ -2,3 +2,24 @@ $().ready(function() {
     // console.log("hello world")
     $('img').show(800)
 })
+function makeThebarsMove(){
+    $('.technic').show(3000)
+    
+}
+document.onmousemove = function() {
+     
+    if(scrollY > 392) {
+        // console.log("you are here")
+        makeThebarsMove()
+     }
+ 
+}
+
+let  clickOnKey = addEventListener('keydown',function(e) {
+    if(scrollY > 392) {
+        // console.log("you are here")
+        makeThebarsMove()
+     }
+ 
+})
+
