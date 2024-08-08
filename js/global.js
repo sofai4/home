e379f940446cc21492ab88b3754e936ea1e083ee
sofia4 <lagosmorgan0@gmail.com>
2024-08-08 19:09:15 -0400

    done

--- js/global.js
+++ js/global.js
@@ -1,1 +1,7 @@
 console.log("i'm the global file")
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-0P29BVB28D');
