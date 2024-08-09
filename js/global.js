4b70134638f002e7515086be39fca25f5dcf390f
sofia4 <lagosmorgan0@gmail.com>
2024-08-09 12:14:57 -0400

    start work on the response website

--- js/global.js
+++ js/global.js
@@ -5,3 +5,30 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-0P29BVB28D');
+
+let navX = document.querySelector('nav .nav-x');
+
+let int = setInterval(() => {
+    console.log($(document).width())
+    if($('nav').width() < 1235 ) {
+        $('nav ul').hide()
+        $('.nav-x').show()
+    }else if($('nav').width() > 1235) {
+        $('nav ul').show()
+        $('.nav-x').hide()
+    }
+}, 90);
+
+navX.onclick = function() {
+    $('nav ul').css({
+        flexDirection: 'column',
+    })
+    $('nav a').css({
+        flexDirection: 'column',
+        marginBottom: '40px'
+    })
+    setTimeout(() => {
+        $('nav ul').toggle()
+        clearInterval(int)
+    }, 80);
+}
