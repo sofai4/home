1a73173dac434017f3e002030ee144152e48e7fc
sofia4 <lagosmorgan0@gmail.com>
2024-08-22 11:55:07 +0100

    blog page function for updating the content

--- js/sofai.js
+++ js/sofai.js
@@ -1,2 +1,40 @@
 // simple animate with jquery
 $('.who ul').slideToggle(900)
+// make statmaent for location
+if(location.href == 'https://sofai4.github.io/home/sofai4h/blog.html?morgan') {
+    //if news for sofai 4h
+    console.log('welcome ' + location.search)
+    // location.href = `https://4h.kesug.com/content.php?content='true'`
+}else if(location.href == 'https://sofai4.github.io/home/sofai4h/blog.html?sofai' ) {
+    // console.log('welcome ' + location.search)
+    function readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
+        }
+        rawFile.send(null);
+    }
+    
+    //usage:
+    readTextFile("../sofai4h/content.json", function(text){
+        var data = JSON.parse(text);
+        console.log(data[0].title);
+        console.log(data[0].p1);
+        console.log(data[0].p2);
+        console.log(data[0].p3);
+    });
+}
+// check the user who want the content 
+if(location.pathname == '/home/sofai4h/blog.html') {
+    if(location.search == '?sofai') {
+
+    }else if(location.search == '?morgan') {
+
+    }else {
+        location.href = 'https://sofai4.github.io/home/error.html'
+    }
+}
