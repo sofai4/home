7a0c936165b2b5567b5fd1e305dcb2504d90a6de
sofia4 <lagosmorgan0@gmail.com>
2024-08-23 15:25:04 +0100

    let done from this page

--- js/sofai.js
+++ js/sofai.js
@@ -1,49 +1,47 @@
-// simple animate with jquery
-$('.who ul').slideToggle(900)
-// make statmaent for location
+// // simple animate with jquery
+// $('.who ul').slideToggle(900)
+// // make statmaent for location
 
-if(localStorage.getItem('stay') == null) {
-    console.log('no')
-    location.href = 'https://4h.kesug.com/blog.php?title=co';
-    localStorage.setItem('stay','true')
-}else {
-    let t = document.querySelector('h1')
-    let p = document.querySelectorAll('p')
-    let video  = document.querySelector('iframe')
-    let links = document.querySelectorAll('#link')
-    let params = new URL(document.location.toString()).searchParams;
-    let t1 = params.get("t");
-    let p1 = params.get("p1");
-    let p2 = params.get("p2");
-    let p3 = params.get("p3");
-    let video1 = params.get("video");
-    let link1 = params.get("link1");
-    let link2 = params.get("link2");
-    let link3 = params.get("link3")
-    if(t1 == null) {
-         location.href = 'https://4h.kesug.com/blog.php?title=co';
-    }
-    t.textContent = t1
-    p[0].textContent = p1
-    p[1].textContent = p2
-    p[2].textContent = p3
-    links[0].setAttribute('href',link1)
-    links[1].setAttribute('href',link2)
-    links[2].setAttribute('href',link3)
-    links[0].textContent = link1
-    links[1].textContent = link2
-    links[2].textContent = link3
-    video.setAttribute('src',video1)
-    localStorage.setItem('t',t1)
-    localStorage.setItem('p1',p1)
-    localStorage.setItem('p2',p2)
-    localStorage.setItem('p3',p3)
-    localStorage.setItem('video',video1)
-    localStorage.setItem('link1',link1)
-    localStorage.setItem('link2',link2)
-    localStorage.setItem('link3',link3)
-}
+// if(localStorage.getItem('stay') == null) {
+//     console.log('no')
+//     location.href = 'https://4h.kesug.com/blog.php?title=co';
+//     localStorage.setItem('stay','true')
+// }else {
+//     let t = document.querySelector('h1')
+//     let p = document.querySelectorAll('p')
+//     let video  = document.querySelector('iframe')
+//     let links = document.querySelectorAll('#link')
+//     let params = new URL(document.location.toString()).searchParams;
+//     let t1 = params.get("t");
+//     let p1 = params.get("p1");
+//     let p2 = params.get("p2");
+//     let p3 = params.get("p3");
+//     let video1 = params.get("video");
+//     let link1 = params.get("link1");
+//     let link2 = params.get("link2");
+//     let link3 = params.get("link3")
+//     if(t1 == null) {
+//          location.href = 'https://4h.kesug.com/blog.php?title=co';
+//     }
+//     t.textContent = t1
+//     p[0].textContent = p1
+//     p[1].textContent = p2
+//     p[2].textContent = p3
+//     links[0].setAttribute('href',link1)
+//     links[1].setAttribute('href',link2)
+//     links[2].setAttribute('href',link3)
+//     links[0].textContent = link1
+//     links[1].textContent = link2
+//     links[2].textContent = link3
+//     video.setAttribute('src',video1)
+//     localStorage.setItem('t',t1)
+//     localStorage.setItem('p1',p1)
+//     localStorage.setItem('p2',p2)
+//     localStorage.setItem('p3',p3)
+//     localStorage.setItem('video',video1)
+//     localStorage.setItem('link1',link1)
+//     localStorage.setItem('link2',link2)
+//     localStorage.setItem('link3',link3)
+// }
 
 
-
-console.log(links)
